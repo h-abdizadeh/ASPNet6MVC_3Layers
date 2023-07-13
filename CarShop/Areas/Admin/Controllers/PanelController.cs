3688e4c42e9531251c56a694fe006e62ee4df605
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2023-07-13 23:34:18 +0330

    2023-7-13_1402-4-22

--- CarShop/Areas/Admin/Controllers/PanelController.cs
+++ CarShop/Areas/Admin/Controllers/PanelController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CarShop.Classes;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarShop.Areas.Admin.Controllers;
 
 [Area("Admin")]
 [Authorize]
+[RoleAttribute("admin")]
 public class PanelController : Controller
 {
     public IActionResult Index()
---
+++ CarShop/Classes/RoleAttribute.cs
@@ -0,0 +1,42 @@
+﻿using CarShop.Database.Context;
+using CarShop.Database.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CarShop.Classes;
+
+public class RoleAttribute : AuthorizeAttribute, IAuthorizationFilter
+{
+
+    private readonly string _roleName;
+
+    public RoleAttribute(string roleName)
+    {
+        _roleName = roleName;
+    }
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var identity = context.HttpContext.User.Identity;
+
+        if (identity.IsAuthenticated)
+        {
+            var userMobile = identity.Name;
+
+            DatabaseContext _context = new DatabaseContext();
+            var user =
+                _context.Users.FirstOrDefault(u => u.Mobile == userMobile && u.Role.RoleName == _roleName);
+
+            if (user == null)
+            {
+                context.Result = new RedirectResult("~/account/login");
+            }
+
+        }
+        else
+        {
+            context.Result = new RedirectResult("~/account/login");
+        }
+    }
+}
